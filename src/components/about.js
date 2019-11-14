c45bb07409b1145c67fdf79a63fd4b8ae233d1b8
Phosawat <phosawat.o@gmail.com>
2019-11-15 01:59:37 +0700

    Update detail for about page

--- src/components/about.js
+++ src/components/about.js
@@ -26,7 +26,15 @@ class About extends Component {
         return (
             <AboutLayout>
                 <InfoLayout>
-                    <TextWithMargit>Hello everyone<br />  This is my Demo website.</TextWithMargit>
+                    <TextWithMargit>
+                        Hello everyone<br />  
+                        This is my Demo website. <br/>
+                        <br/>
+                        On /SCG page you will see 2 column.<br/>
+                        First is column that you can fill anything after you fill value, it will send your input to search value from server and display results on UI. <br/>
+                        <br/>
+                        Second column, you still can fill anything like the first column but it will return list of restaurants in the name of area that you fill.
+                    </TextWithMargit>
                     <MyText>Enjoy</MyText>
                 </InfoLayout>
                 <iframe title="resume" src="https://drive.google.com/file/d/1ca3m0QGNqL1LELrUAV6ryrhlMQ0neh4d/preview" width="600" height="800"></iframe>
